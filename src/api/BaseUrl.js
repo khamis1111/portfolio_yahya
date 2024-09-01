5f0fe3a93acc488b4ee049cffedfc2a8ec6449fc
unknown <mahmood@gmail.com>
2024-09-01 21:21:42 +0300

    update

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const urlWeb = "http://127.0.0.1:8080";
-// export const urlWeb = "https://portfolio-yaya-backend.vercel.app";
+// export const urlWeb = "http://127.0.0.1:8080";
+export const urlWeb = "https://portfolio-yaya-backend.vercel.app";
 const baseUrl = axios.create({ baseURL: urlWeb });
 
 export default baseUrl;
